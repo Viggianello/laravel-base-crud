6f835e55915acff230d9c2015bc6e12f7d9974c4
Antonello Viggiano <antonelloviggiano1990@hotmail.it>
2020-07-09 16:26:22 +0200

    working progress

---
+++ database/seeds/ProductsTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+     public function run(Faker $faker)
+     {
+         for ($i=0; $i < 10; $i++) {
+             $new_product = new Product();
+             $new_product->name = $faker->word;
+             $new_product->description = $faker->sentence(10);
+             $new_product->price = $faker->randomFloat(2, 10, 1000);
+             $new_product->save();
+         }
+     }
+ }
